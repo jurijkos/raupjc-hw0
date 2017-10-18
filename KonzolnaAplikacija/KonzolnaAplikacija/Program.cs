bdc2b8ccf1261fd88be840e1a729f1a844938eb5
Jurij <jurij.kos1996@live.com>
2017-10-18 23:40:09 +0200

    commit

---
+++ KonzolnaAplikacija/KonzolnaAplikacija/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KonzolnaAplikacija
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.ReadLine();
+            MyConsole.Class1.PrintHelloWorld();
+
+        }
+    }
+}
---
+++ KonzolnaAplikacija/MyConsole/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyConsole
+{
+    public class Class1
+    {
+        public static void PrintHelloWorld()
+        {
+            Console.WriteLine("hello jurij");
+            Console.ReadLine();
+        }
+    }
+}
